780107fa6a1aef28f1318fc77ab56f57c075641b
OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>
2023-05-27 14:03:17 -0300

    Modelo de Guia

--- src/main/java/com/sistemadecadastro/cadastrodeguias/model/Guia.java
+++ src/main/java/com/sistemadecadastro/cadastrodeguias/model/Guia.java
@@ -1,2 +1,38 @@
-package com.sistemadecadastro.cadastrodeguias;public class Guia {
+package com.sistemadecadastro.cadastrodeguias.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="guias")
+@Data
+public class Guia {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+    private String sus;
+    private String procedimento;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate data_nascimento;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate data_recebimento;
+
+    public Guia(String nome, String sus, String procedimento, LocalDate data_nascimento, LocalDate data_recebimento) {
+        this.nome = nome;
+        this.sus = sus;
+        this.procedimento = procedimento;
+        this.data_nascimento = data_nascimento;
+        this.data_recebimento = data_recebimento;
+    }
+    public Guia() {
+
+    }
 }
+
