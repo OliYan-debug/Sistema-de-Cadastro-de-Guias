b2ece340d2160b81810d3e26969233da3ec5668c
OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>
2023-06-02 22:15:03 -0300

    Update agora aceita somente um parametro sem tornar os outros nulos #5
    Principais mudanças:
     - no Controller `@RequestParam` no lugar do `@RequestBody` com todos os
       campos opcionais e agora invés do Service retornar nulo vai retornar
    exceção no `updateGuia`. troquei `@RequestParam` por `@PathVariable` no
    remove guia para padronizar
     - no Model foram adicionados as Annotations `@Size` e `@Column`
     - no Service o `updateGuia` foi alterado para atualizar todos os campos
       passados no controller desde que sejam válidos
     - no `pom.xml` adicionado o Spring boot validation

--- src/main/java/com/sistemadecadastro/cadastrodeguias/model/Guia.java
+++ src/main/java/com/sistemadecadastro/cadastrodeguias/model/Guia.java
@@ -1,12 +1,10 @@
 package com.sistemadecadastro.cadastrodeguias.model;
 
 
-import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
-
 import org.springframework.format.annotation.DateTimeFormat;
-
 
 import java.time.LocalDate;
 
@@ -17,20 +15,28 @@ public class Guia {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Size(min = 3)
+    @Column(name="nome", nullable = false)
     private String nome;
+    @Size(min = 15, max = 15)
+    @Column(name="sus", nullable = false, length = 15)
     private String sus;
+    @Size(min = 2)
+    @Column(name = "procedimento", nullable = false)
     private String procedimento;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private LocalDate data_nascimento;
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private LocalDate data_recebimento;
+    @Column(name = "data_recebimento", nullable = false)
+    private LocalDate dataRecebimento;
 
-    public Guia(String nome, String sus, String procedimento, LocalDate data_nascimento, LocalDate data_recebimento) {
+    public Guia(String nome, String sus, String procedimento, LocalDate dataNascimento, LocalDate dataRecebimento) {
         this.nome = nome;
         this.sus = sus;
         this.procedimento = procedimento;
-        this.data_nascimento = data_nascimento;
-        this.data_recebimento = data_recebimento;
+        this.dataNascimento = dataNascimento;
+        this.dataRecebimento = dataRecebimento;
     }
     public Guia() {
 
